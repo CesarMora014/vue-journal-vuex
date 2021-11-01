02ec7a8e3bc3233a36442edc2d30eb824d91c2d7
cesarmora014 <cesarmora014@gmail.com>
2021-11-01 12:47:18 -0600

    CURD

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -6,7 +6,7 @@ const actions = {
         const { data } = await journalAPI.get('/entries.json')
         const entries = []
 
-        for( let id  of Object.keys(data) ){
+        for( let id of Object.keys(data) ){
             entries.push({
                 id,
                 ...data[id]
